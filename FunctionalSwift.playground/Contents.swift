b694a406558cead37a68f7f27720503844a73090
derpoliuk <derpoliuk@gmail.com>
2016-11-23 13:33:10 +0200

    Add original code for Ship

--- FunctionalSwift.playground/Contents.swift
+++ FunctionalSwift.playground/Contents.swift
@@ -2,4 +2,54 @@
 
 import UIKit
 
-var str = "Hello, playground"
+typealias Distance = Double
+
+struct Position {
+    var x: Double
+    var y: Double
+}
+
+extension Position {
+    func inRange(_ range: Distance) -> Bool {
+        return sqrt(x * x + y * y) <= range
+    }
+}
+
+struct Ship {
+    var position: Position
+    var firingRange: Distance
+    var unsafeRange: Distance
+}
+
+extension Ship {
+    func canEngage(ship: Ship) -> Bool {
+        let dx = ship.position.x - position.x
+        let dy = ship.position.y - position.y
+        let shipDistance = sqrt(dx * dx + dy * dy)
+        return shipDistance <= firingRange
+    }
+}
+
+extension Ship {
+    func canSafelyEngage(ship: Ship) -> Bool {
+        let dx = ship.position.x - position.x
+        let dy = ship.position.y - position.y
+        let targetDistance = sqrt(dx * dx + dy * dy)
+        return targetDistance <= firingRange
+            && targetDistance > unsafeRange
+    }
+}
+
+extension Ship {
+    func canSafelyEngage1(ship: Ship, friendly: Ship) -> Bool {
+        let dx = ship.position.x - position.x
+        let dy = ship.position.y - position.y
+        let targetDistance = sqrt(dx * dx + dy * dy)
+        let friendlyDx = friendly.position.x - ship.position.x
+        let friendlyDy = friendly.position.y - ship.position.y
+        let friendlyDistance = sqrt(friendlyDx * friendlyDx + friendlyDy * friendlyDy)
+        return targetDistance <= firingRange
+            && targetDistance > unsafeRange
+            && friendlyDistance > unsafeRange
+    }
+}
